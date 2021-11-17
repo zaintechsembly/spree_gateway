6136d5c28ac1c215ed8d48c2d20c80a0be04c696
userzee <zeeshan@plerosys.com>
2021-11-17 21:48:04 +0500

    use different order reference in description

--- app/models/spree/gateway/adyen_gateway.rb
+++ app/models/spree/gateway/adyen_gateway.rb
@@ -85,8 +85,7 @@ module Spree
 
     def options_for_purchase_or_auth(money, creditcard, gateway_options)
       options = { recurring: false }
-      reference = gateway_options[:order_id]
-      options[:order_id] = reference
+      options[:order_id] = gateway_options[:order_reference_id]
       options[:currency] = gateway_options[:currency]
       options[:return_url] = "api/v2/storefront/checkout?order_token=#{gateway_options[:order_token]}"
       options[:channel] = "web"
--- lib/spree_gateway/version.rb
+++ lib/spree_gateway/version.rb
@@ -1,5 +1,5 @@
 module SpreeGateway
   def self.version
-    '3.8.5'
+    '3.8.6'
   end
 end
