9fab9ca72ac3d8f45297cf4670e7d03395f6625c
Hasham Sabir <hashamsabir@192.168.88.191>
2023-02-07 12:34:14 +0500

    refactor code

--- app/models/spree/gateway/paypal_capture.rb
+++ app/models/spree/gateway/paypal_capture.rb
@@ -17,7 +17,11 @@ module Spree
     end
 
     def source_not_found
-      {success: false, message: 'source not found'}
+      json_response('source not found')
+    end
+
+    def json_response(message, success = false, response = nil )
+      { success: success, message: message, response: response }
     end
 
     def capture_order
@@ -26,26 +30,26 @@ module Spree
                 "Content-Type": 'application/json',
                 "Authorization": "Bearer #{ @auth['access_token'] }"
               }
-            );
-      { success: response.success?, message: response.message }
+            )
+      json_response(response.message, response.success?, response)
     rescue => e
       Rails.logger.error(e.message)
-      { success: false, message: e.message }
+      json_response(e.message)
     end
 
     def authorization
-      basicAuth = Base64.strict_encode64("#{ preferred_client_id }:#{ preferred_client_secret }");
+      basicAuth = Base64.strict_encode64("#{ preferred_client_id }:#{ preferred_client_secret }")
       response = HTTParty.post("#{paypal_api}/v1/oauth2/token/", 
           headers: {
             "Content-Type": 'application/x-www-form-urlencoded',
             "Authorization": "Basic #{ basicAuth }"
           },
           body: "grant_type=client_credentials"
-        );
-        response.parsed_response
+        )
+      response.parsed_response
     rescue => e
       Rails.logger.error(e.message)
-      { success: false, message: e.message }
+      json_response(e.message)
     end
 
   end
